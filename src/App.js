5bec66c9e16ae140fd4fb0141df4caa52b334574
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-04-12 13:57:03 -0500

    created react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+  <body>
+    <Header/>
+  </body>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Header() {
+  return (
+  <header>
+    <p>header</p>
+  </header>
+  );
+}
+
+export default Header;
