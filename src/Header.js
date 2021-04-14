82d0eacca78850adbed21597fbf01b9b821548f2
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-04-14 10:30:42 -0500

    completed navbar

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,22 @@
 import './App.css';
+import './css/nav.css';
+import { FaSquareFull } from "react-icons/fa";
 
 function Header() {
+
   return (
-  <header>
-    <p>header</p>
-  </header>
+  <>
+    <header id="nav-bar">
+        <div id="name"><h1>jakobsPortfolio</h1><FaSquareFull id="squareName"/></div>
+        <ul>
+          <a href="/"><li>Home<FaSquareFull className="squareNav"/></li></a>
+          <a href="/"><li>About<FaSquareFull className="squareNav"/></li></a>
+          <a href="/"><li>Skills<FaSquareFull className="squareNav"/></li></a>
+          <a href="/"><li>Projects<FaSquareFull className="squareNav"/></li></a>
+          <a href="/"><li>Contact<FaSquareFull className="squareNav"/></li></a>
+        </ul>
+    </header>
+  </>
   );
 }
 
