0a312973e6ded47df7e26bd3442d23b5f539f5ba
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-04-16 22:03:54 -0500

    added links section

--- src/Links/Links.js
+++ src/Links/Links.js
@@ -2,21 +2,39 @@ import '../css/welcomepage.css';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import { IoChevronForwardOutline } from "react-icons/io5";
+import newe from './img/newe.png';
+import newCheck from './img/newCheck.png';
+
 
 function Links() {
   return (
     <section id="LinksS">
       <Container id="theContainer">
-      <Row id="CardRow">
-          <Card id="ExperenceCard">
-              <Card.Text>
-                Hello
-              </Card.Text>
+           <Row id="CardRow">
+            <Card id="ExperenceCard">
+      <a href="/"><div className="insideCard">
+                <div id="backgroundEmblem">
+                    <img src={newe} height="50px" alt="dwdr3rr"/>
+                </div>
+                <div className="rightExperience">
+                  <h2>1+</h2>
+                  <p>Years of Experience</p>
+                </div>
+                <IoChevronForwardOutline className="arrowIcon"/>
+              </div></a>
           </Card>
           <Card id="ProjectCard">
-              <Card.Text>
-                Hello
-              </Card.Text>
+           <a href="/"><div className="insideCard">
+              <div id="backgroundCheck">
+                  <img src={newCheck} alt="dwd"/>
+              </div>
+              <div className="rightExperience">
+                  <h2>1+</h2>
+                  <p>Completed Projects</p>
+                </div>
+                <IoChevronForwardOutline className="arrowIcon"/>
+            </div></a>
           </Card>
         </Row>
       </Container>  
