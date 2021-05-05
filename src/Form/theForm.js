e70c94b0973161ec6e66ed97d66860d537a3ea2d
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-05-04 22:18:23 -0500

    working on form

--- src/Form/theForm.js
+++ src/Form/theForm.js
@@ -1,17 +1,48 @@
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
+import{ init } from 'emailjs-com';
+init("user_HbXSwUrhvxqgOmDqO82UD");
+import { FaUserAlt } from 'react-icons/fa';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function TheForm() {
+  const sendemail = (e) => {
+    e.preventDefault();
+    var objP={
+      name: e.target[0].value,
+      email: e.target[1].value,
+      phone: e.target[2].value,
+      message: e.target[3].value
+    }
+    
+    console.log(e.target[0]);
+    emailjs.send('service_9r8qakp', 'contact_form', objP)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      //empty the value
+      e.target[0].value="";
+      e.target[1].value="";
+      e.target[2].value="";
+      e.target[3].value="";
+  }
   return (
-    <Form id="formforMessage">
+    <Form id="formforMessage" onSubmit={sendemail}>
       <Col>
           <Form.Group className="theGroup">
             <Form.Label className="textEdit">Name</Form.Label>
-            <Form.Control 
+            <Form.Control
+               <InputGroup.Prepend>
+               <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+               </InputGroup.Prepend>
               type="text"
               name="name"
-              placeholder="e.g. John Doe"/>
+              placeholder="e.g. John Doe" required/>
           </Form.Group>
 
           <Form.Group className="theGroup">
@@ -19,7 +50,7 @@ function TheForm() {
             <Form.Control 
               type="text"
               name="email"
-              placeholder="e.g. john.doe@gmail.com"/>
+              placeholder="e.g. john.doe@gmail.com" required/>
           </Form.Group>
 
           <Form.Group className="theGroup">
@@ -37,10 +68,10 @@ function TheForm() {
               id="messageSection"
               type="text"
               name="message"
-              placeholder="Write message..."/>
+              placeholder="Write message..." required/>
           </Form.Group>
 
-          <Button>SEND</Button>
+          <Button type="submit">SEND</Button>
       </Col>
     </Form>
   );
